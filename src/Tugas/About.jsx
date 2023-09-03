c3618ef15a3f8d10131bee2c72c6dac6e53227cf
jeffpsrb <jeffreyp454r1bu@gmail.com>
2023-09-03 17:12:14 +0700

    First Commit

---
+++ src/Tugas/About.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function About() {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>Selamat datang di halaman About!</p>
+      <br />
+      <h3>Hi nama saya Jeffrey Jeverson Pasaribu</h3>
+    </div>
+  );
+}
+
+export default About;
---
+++ src/Tugas/Contact.jsx
@@ -0,0 +1,15 @@
+// src/components/Home.js
+import React from 'react';
+
+function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>Selamat datang di halaman contact!</p>
+      <br />
+      <h3>Anda dapat menghubungi saya melalui emal jeffreyp454r1bu@gmail.com</h3>
+    </div>
+  );
+}
+
+export default Contact;
